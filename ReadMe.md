95aef6c455258b1015955ceb24bf57453390606c
Flombo <florian-pfuetzenreuter@hotmail.de>
2021-12-04 18:38:59 +0100

    Wrtoe minimal functioning pipeline.

---
+++ ReadMe.md
@@ -0,0 +1,27 @@
+#Instructions
+
+1. Kafka Cheatsheet 
+   ```
+   Init
+   bin/zookeeper-server-start.sh config/zookeeper.properties
+   bin/kafka-server-start.sh config/server.properties 
+   
+   Create topic 
+   bin/kafka-topics.sh --create --topic 2-2 --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1 
+   
+   Read topic
+   bin/kafka-console-consumer.sh --topic 2-1 --from-beginning --bootstrap-server localhost:9092 
+   
+   List active topics
+   bin/kafka-topics.sh --list --bootstrap-server localhost:9092 
+   
+   Delete topic
+   bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic test2 
+   
+   Delete multiple topics with regex
+   bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic '2-.*'
+   ```
+
+2. Pipeline execution
+   1. Go to '~/Projects/Java/'
+   2. Run 'mvn -X compile exec:java -Dexec.mainClass=java/Main'
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,118 +1,34 @@
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.io.kafka.KafkaIO;
-import org.apache.beam.sdk.options.*;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.transforms.Values;
-import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.io.kafka.KafkaRecord;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.joda.time.Duration;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
 
 public class Main {
 
+    public static void main(String[] args) {
 
-    /**
-     * Specific pipeline options.
-     */
-    private interface Options extends PipelineOptions {
-        @Description("Kafka Bootstrap Servers")
-        @Default.String("localhost:9092")
-        String getKafkaServer();
+        try {
+            Pipeline pipeline = Pipeline.create();
 
-        void setKafkaServer(String value);
+            PCollection<KafkaRecord<String, String>> data = pipeline.apply(
+                    KafkaIO.<String, String>read()
+                            .withBootstrapServers("localhost:9092")
+                            .withTopic("2-1")
+                            .withKeyDeserializer(StringDeserializer.class)
+                            .withValueDeserializer(StringDeserializer.class)
+            );
 
-        @Description("Kafka Topic Name")
-        @Default.String("2-1")
-        String getInputTopic();
+            System.out.println(data.toString());
 
-        void setInputTopic(String value);
+            PipelineResult run = pipeline.run();
 
-        @Description("Kafka Output Topic Name")
-        @Default.String("2-2")
-        String getOutputTopic();
-
-        void setOutputTopic(String value);
-
-        @Description("Pipeline duration to wait until finish in seconds")
-        @Default.Long(-1)
-        Long getDuration();
-
-        void setDuration(Long duration);
-
-        class GDELTFileFactory implements DefaultValueFactory<String> {
-            public String create(PipelineOptions options) {
-                SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
-                return format.format(new Date());
-            }
+            run.waitUntilFinish(Duration.standardSeconds(-1));
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        System.out.println(args);
-        Options options = PipelineOptionsFactory.fromArgs(args)
-                .withValidation().as(Options.class);
-
-        System.out.println(options.toString());
-        Pipeline pipeline = Pipeline.create(options);
-
-        // now we connect to the queue and process every event
-        PCollection<String> data = pipeline.apply(
-                "ReadFromKafka",
-                KafkaIO.<String, String> read()
-                        .withBootstrapServers(options.getKafkaServer())
-                        .withTopics(
-                                Collections.singletonList(options
-                                        .getInputTopic()))
-                        .withoutMetadata()).apply("ExtractPayload",
-                Values.<String> create());
-
-        data.apply(ParDo.of(new DoFn<String, String>() {
-            @ProcessElement
-            public void processElement(ProcessContext c) {
-                System.out.println(String.format("** element |%s| **",
-                        c.element()));
-            }
-        }));
-        // We filter the events for a given country (IN=India) and send them to their own Topic
-        final String country = "IN";
-        PCollection<String> eventsInIndia = data.apply("FilterByCountry",
-                ParDo.of(new DoFn<String, String>() {
-                    @ProcessElement
-                    public void processElement(ProcessContext c) {
-                        //                    if (getCountry(c.element()).equals(country)){
-                        c.output(c.element());
-                        //                    }
-
-                    }
-                }));
-
-        PCollection<KV<String, String>> eventsInIndiaKV = eventsInIndia
-                .apply("ExtractPayload",
-                        ParDo.of(new DoFn<String, KV<String, String>>() {
-                            @ProcessElement
-                            public void processElement(ProcessContext c)
-                                    throws Exception {
-                                c.output(KV.of("india", c.element()));
-                            }
-                        }));
-
-        eventsInIndiaKV
-                .apply("WriteToKafka",
-                        KafkaIO.<String, String> write()
-                                .withBootstrapServers(
-                                        options.getKafkaServer())
-                                .withTopic(options.getOutputTopic())
-                                .withKeySerializer(
-                                        org.apache.kafka.common.serialization.StringSerializer.class)
-                                .withValueSerializer(
-                                        org.apache.kafka.common.serialization.StringSerializer.class));
-        PipelineResult run = pipeline.run();
-        run.waitUntilFinish(Duration.standardSeconds(options.getDuration()));
     }
 
 }
