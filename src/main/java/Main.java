df96461938e03463ebf90280f189240ab66c0d55
BizarreBits <63097150+BizarreBits@users.noreply.github.com>
2021-12-05 19:52:18 +0100

    First working transition

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,10 @@ import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.io.kafka.KafkaIO;
 import org.apache.beam.sdk.io.kafka.KafkaRecord;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.joda.time.Duration;
 
@@ -13,21 +16,34 @@ public class Main {
         try {
             Pipeline pipeline = Pipeline.create();
 
-            PCollection<KafkaRecord<String, String>> data = pipeline.apply(
-                    KafkaIO.<String, String>read()
+            PCollection<KafkaRecord<Long, String>> data = pipeline.apply(
+                    KafkaIO.<Long, String>read()
                             .withBootstrapServers("localhost:9092")
                             .withTopic("2-1")
-                            .withKeyDeserializer(StringDeserializer.class)
+                            .withKeyDeserializer(LongDeserializer.class)
                             .withValueDeserializer(StringDeserializer.class)
             );
 
-            System.out.println(data.toString());
+
+            PCollection<String> output;
+            output = data.apply(ParDo.of(new Print()));
+
 
             PipelineResult run = pipeline.run();
 
             run.waitUntilFinish(Duration.standardSeconds(-1));
         } catch (Exception exception) {
             exception.printStackTrace();
+        }
+    }
+
+    public static class Print extends DoFn<KafkaRecord<Long,String>, String>
+    {
+        @ProcessElement
+        public void processElement(@Element KafkaRecord<Long,String> input, OutputReceiver<String>out)
+        {
+            System.out.println(input.getKV().getValue());
+            out.output(input.getKV().getValue());
         }
     }
 
