6ba6640202831a63e373ffcbc7a1f70161a56da8
Flombo <florian-pfuetzenreuter@hotmail.de>
2022-01-19 20:44:07 +0100

    Changed RetrieveProcessingTimestampHelper

--- src/main/java/Transformations/RetrieveProcessingTimestampHelper.java
+++ src/main/java/Transformations/RetrieveProcessingTimestampHelper.java
@@ -1,14 +1,12 @@
 package Transformations;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class RetrieveProcessingTimestampHelper {
 
     public static String retrieveTimestamp() {
         LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        return now.format(format);
+        return now.toString();
     }
 
 }
